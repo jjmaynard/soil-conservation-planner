b9d9c0368296e6e197320ba8cec595af589e4bba
jjmaynard <jmaynard@nmsu.edu>
2025-12-16 09:29:51 -0800

    Add custom hexagonal favicon with soil profile layers

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,10 @@ import { Head, Html, Main, NextScript } from 'next/document'
 
 const Document = () => (
   <Html lang="en">
-    <Head />
+    <Head>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+      <link rel="alternate icon" href="/favicon.ico" />
+    </Head>
     <body className="bg-white">
       <Main />
       <NextScript />
